f18b8a8bc9bf8ebb897d4dff46f597de9c88400a
Maicon de Souza Alcântara <maicon-s-a@hotmail.com>
2022-05-23 22:41:57 -0400

    devops: test

--- terraform/modules/code-build/main.tf
+++ terraform/modules/code-build/main.tf
@@ -38,6 +38,7 @@ resource "aws_iam_role_policy" "role-policy" {
             "Effect": "Allow",
             "Resource": [
                 "arn:aws:s3:::codepipeline-us-east-1-*"
+                
             ],
             "Action": [
                 "s3:PutObject",
